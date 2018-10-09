866daa682d9dff47c83ddf131b49f0a06f6ac9c6
Sam Oteo <tortillabar@me.com>
2018-10-09 15:35:46 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
-
+#just a comment
 class Dancer
   include Dance
   extend MetaDancing
